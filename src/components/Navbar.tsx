83255017fb0fca2ef3de3a1d2e21c54f7c2d771c
A.Kaushik <a.kaushik3003@gmail.com>
2026-05-20 00:48:33 +0530

    made navbar responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,27 +18,26 @@ export default function Navbar() {
       transition={{ duration: 0.8 }}
       className="fixed top-0 left-0 w-full z-50 backdrop-blur-xl bg-white/70 border-b border-gray-200 shadow-sm"
     >
-
-      <div className="max-w-7xl mx-auto flex justify-between items-center px-6 py-5">
+      <div className="max-w-7xl mx-auto flex justify-between items-center px-5 md:px-6 py-4">
 
         {/* Logo */}
-        <h1 className="text-2xl font-bold bg-gradient-to-r from-cyan-500 to-purple-600 bg-clip-text text-transparent">
+        <h1 className="text-2xl md:text-3xl font-bold bg-gradient-to-r from-cyan-500 to-purple-600 bg-clip-text text-transparent">
           A.Kaushik
         </h1>
 
-        {/* Nav Links */}
-        <div className="flex items-center gap-8">
+        {/* Desktop Nav */}
+        <div className="hidden md:flex items-center gap-8">
 
           {navItems.map((item, index) => (
             <a
-  key={index}
-  href={item.link}
-  className="relative text-gray-700 hover:text-cyan-500 transition duration-300 font-medium group"
->
-  {item.name}
+              key={index}
+              href={item.link}
+              className="relative text-gray-700 hover:text-cyan-500 transition duration-300 font-medium group"
+            >
+              {item.name}
 
-  <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-cyan-500 transition-all duration-300 group-hover:w-full"></span>
-</a>
+              <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-cyan-500 transition-all duration-300 group-hover:w-full"></span>
+            </a>
           ))}
 
           {/* Resume Button */}
@@ -52,8 +51,12 @@ export default function Navbar() {
 
         </div>
 
-      </div>
+        {/* Mobile Menu Icon */}
+        <div className="md:hidden text-3xl text-black cursor-pointer">
+          ☰
+        </div>
 
+      </div>
     </motion.nav>
   );
 }
